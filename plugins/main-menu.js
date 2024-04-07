09a9f7e2d3819e27863749ef62fa20f85ef2d8c6
Hjfjckb <158020655+Hjfjckb@users.noreply.github.com>
2024-04-07 11:13:58 +0000

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -1,5 +1,4 @@
-   import { createHash } from 'crypto'
-import PhoneNumber from 'awesome-phonenumber'
+import { createHash } from 'crypto'
 import { canLevelUp, xpRange } from '../lib/levelling.js'
 import fetch from 'node-fetch'
 import fs from 'fs'
@@ -11,29 +10,31 @@ const time = moment.tz('Egypt').format('HH')
 let wib = moment.tz('Egypt').format('HH:mm:ss')
 //import db from '../lib/database.js'
 
-let handler = async (m, { conn, usedPrefix, command}) => {
+let handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, isPrems}) => {
     let d = new Date(new Date + 3600000)
-    let locale = 'en'
+    let locale = 'ar'
     let week = d.toLocaleDateString(locale, { weekday: 'long' })
     let date = d.toLocaleDateString(locale, { day: 'numeric', month: 'long', year: 'numeric' })
     let _uptime = process.uptime() * 1000
     let uptime = clockString(_uptime)
-let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-if (!(who in global.db.data.users)) throw `✳️ The user is not found in my database`
-let pp = '.Menu.png'
-let user = global.db.data.users[who]
-let { name, exp, diamond, lastclaim, registered, regTime, age, level, role, warn } = global.db.data.users[who]
-let { min, xp, max } = xpRange(user.level, global.multiplier)
-let username = conn.getName(who)
-let math = max - xp
-let prem = global.prems.includes(who.split`@`[0])
-let sn = createHash('md5').update(who).digest('hex')
-let rtotalreg = Object.values(global.db.data.users).filter(user => user.registered == true).length 
-let more = String.fromCharCode(8206)
-let readMore = more.repeat(850) 
-let taguser = '@' + m.sender.split("@s.whatsapp.net")[0]
-let str = `
-*❅ ━━━━ » ✥ «﷽» ✥ « ━━━━ ❅
+    let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+    if (!(who in global.db.data.users)) throw `✳️ The user is not found in my database`
+    let videoUrl = 'https://telegra.ph/file/772acd48a378af8d390af.mp4';
+    let vn = '.Menu.png';
+    const user = global.db.data.users[m.sender];
+    const {money, joincount} = global.db.data.users[m.sender];
+    const {exp, limit, level, role} = 
+        global.db.data.users[m.sender];
+    let { min, xp, max } = xpRange(user.level, global.multiplier)
+    let username = conn.getName(who)
+    let math = max - xp
+    let sn = createHash('md5').update(who).digest('hex')
+    let totalreg = Object.keys(global.db.data.users).length;
+    let rtotalreg = Object.values(global.db.data.users).filter(user => user.registered == true).length 
+    let more = String.fromCharCode(8206)
+    let readMore = more.repeat(900) 
+    const taguser = '@' +  m.sender.split('@s.whatsapp.net')[0];
+    let str = `*❅ ━━━━ » ✥ «﷽» ✥ « ━━━━ ❅
 *│❑↫  الـوقـت : ${wib}*
 *│❑↫  الـتاريـخ : ${date}*
 *❅ ━━━━ » ✥ «﷽» ✥ « ━━━━ ❅
@@ -165,36 +166,44 @@ let str = `
 *│ده :https://www.youtube.com/@Kurosaki67383*
 *│*
  ❐─━──━〘•⊰❉⊱•〙•━──━─❐
-`
+    `.trim();
 
-    conn.sendFile(m.chat, pp, 'perfil.jpg', str, false, { mentions: [who] })
+    
 
-}
+    conn.sendMessage(m.chat, {
+        video: { url: VideoUrl }, caption: str,
+        mentions: [m.sender,global.conn.user.jid],
+        gifPlayback: true,gifAttribution: 0
+    }, { quoted: m });
+}; 
+
 handler.help = ['main']
 handler.tags = ['group']
-handler.command =['المهام','الاوامر','اوامر','مهام','menu'] 
+handler.command = ['اوامر'] 
 
 export default handler
+
 function clockString(ms) {
     let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
     let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
     let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
-    return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')}
-    
-    function ucapan() {
-      const time = moment.tz('Egypt').format('HH')
-      let res = "بداية يوم سعيده ☀️"
-      if (time >= 4) {
+    return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')
+}
+
+function ucapan() {
+    const time = moment.tz('Egypt').format('HH')
+    let res = "بداية يوم سعيده ☀️"
+    if (time >= 4) {
         res = "صباح الخير 🌄"
-      }
-      if (time >= 10) {
-        res = "مساء الخير ☀️"
-      }
-      if (time >= 15) {
-        res = "مساء الخير 🌇"
-      }
-      if (time >= 18) {
-        res = "مساء الخير 🌙"
-      }
-      return res
     }
+    if (time >= 10) {
+        res = "مساء الخير ☀️"
+    }
+    if (time >= 15) {
+        res = "مساء الخير 🌇"
+    }
+    if (time >= 18) {
+        res = "مساء الخير 🌙"
+    }
+    return res
+       }
