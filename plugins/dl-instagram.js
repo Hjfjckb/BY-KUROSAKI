fba9f5f635657295eb2fe2e1e6b6ffc236d7308c
Hjfjckb <158020655+Hjfjckb@users.noreply.github.com>
2024-04-07 10:25:23 +0000

    Add files via upload

---
+++ plugins/dl-instagram.js
@@ -0,0 +1,27 @@
+import fetch from 'node-fetch';
+
+let handler = async (m, { conn, usedPrefix, args, command, text }) => {
+  if (!text) throw `*أعــطيـني رابــــط !*`;
+
+  let res;
+  try {
+    res = await fetch(`https://inrl-web.onrender.com/api/insta?url=${text}`);
+  } catch (error) {
+    throw `*حـدث خــطأ !:* ${error.message}`;
+  }
+
+  let api_response = await res.json();
+  if (!api_response || !api_response.result || api_response.result.length === 0) {
+    throw `*لــم اجـد الـفيديــو !!*`;
+  }
+
+  let cap = `*الـفيديــو تـفضـل*`;
+
+  conn.sendFile(m.chat, api_response.result[0], 'instagram.mp4', cap, m);
+}
+
+handler.help = ['instagram']
+handler.tags = ['downloader']
+handler.command = /^(instagram|انستا|ig|instagramdl)$/i
+
+export default handler
---
+++ plugins/dl-yta.js
@@ -0,0 +1,34 @@
+import { youtubedl, youtubedlv2 } from '@bochilteam/scraper';
+
+let handler = async (m, { conn, text, args, isPrems, isOwner, usedPrefix, command }) => {
+  if (!args || !args[0]) throw `*مــثال :\n${usedPrefix + command} https://youtu.be/YzkTFFwxtXI*`;
+  if (!args[0].match(/youtu/gi)) throw `❎ تـأكـد أنـه رابـط يــوتيـوب !`;
+
+
+  try {
+    let q = '128kbps'; 
+    let v = args[0]; 
+    const yt = await youtubedl(v).catch(async () => await youtubedlv2(v)); 
+    const dl_url = await yt.audio[q].download(); 
+    const title = await yt.title; 
+
+    conn.sendFile(
+      m.chat,
+      dl_url,
+      title + '.mp3',
+      null, 
+      m,
+      false,
+      { mimetype: 'audio/mpeg' }
+    );
+
+  } catch {
+    await m.reply(`❎ لـم اسـتطيع تـحمـيل الـملـف !`)
+  }
+};
+
+handler.help = ['ytmp3 <url>']
+handler.tags = ['dl']
+handler.command = ['اغنيه', 'اغنية'] 
+
+export default handler
